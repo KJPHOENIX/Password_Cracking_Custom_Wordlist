c7eb8f05e7ba9056603db1675a0a2d0f8548fb92
KJPHOENIX <140408460+KJPHOENIX@users.noreply.github.com>
2023-08-27 19:11:32 +0530

    Update pasword_carck_custom_wordlist.py

--- pasword_carck_custom_wordlist.py
+++ pasword_carck_custom_wordlist.py
@@ -1,7 +1,20 @@
 from itertools import permutations
- 
+import time
+
+banner = """
+ _  __   _       ____  _   _  ___  _____ _   _ _____  __
+| |/ /  | |     |  _ \| | | |/ _ \| ____| \ | |_ _\ \/ /
+| ' /_  | |_____| |_) | |_| | | | |  _| |  \| || | \  / 
+| . \ |_| |_____|  __/|  _  | |_| | |___| |\  || | /  \ 
+|_|\_\___/      |_|   |_| |_|\___/|_____|_| \_|___/_/\_\
+                                                        
+"""
+time.sleep(2)
+
+print(banner)
 def gen(): 
      # Get all permutations of string 'ABC'
+     time.sleep(2)
      str = input("Enter the Word : ")
      a = int(input("Enter Max length :"))
      permList = permutations(str,a)
